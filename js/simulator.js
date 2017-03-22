7b4d5c455e7d11e4728d6757c0ac1774b382fb25
asanchez <jaehoo@gmail.com>
2017-03-21 21:22:43 -0600

    Change table render to use w2ui

--- js/simulator.js
+++ js/simulator.js
@@ -1,9 +1,14 @@
 var SIM = SIM || {};
  
 SIM.calculate = function () {
- 
- 
-    $('#result .montly').html('');
+
+ if( w2ui['anualGrid'] ){
+     console.info(w2ui['anualGrid']);
+        SIM.reset();
+    }
+    
+    //$('#result .montly').html('');
+
     var age = $('#mainData input[name="age"]');
     var yearsToWork = $('#mainData input[name="working"]');
  
@@ -14,6 +19,7 @@ SIM.calculate = function () {
     var tasa = $('#mainData input[name="tasa"]');
     var anualSaving = $('#mainData input[name="anualSaving"]');
     var taxes = $('#mainData input[name="taxes"]');
+    var initialMonth = $('#month');
 
     
     var inflation= $('#inflacion');
@@ -27,39 +33,85 @@ SIM.calculate = function () {
         'montlySaving': montlySaving.val(),
         'anualSaving':anualSaving.val(),
         'taxes':taxes.val(),
-        'inflation': inflation.val()
+        'inflation': inflation.val(),
+        'initialMonth': initialMonth.val()
  
  
     };
  
     //alert(inputData);
+    
+    var anualGrid = $('#anualGrid');
+    var montlyGrid = $('#monthlyGrid');
+
+    var data = SIM.simulation(inputData);
+
+    anualGrid.w2grid({
+        name: 'anualGrid',
+        header: 'Finantial simulator',
+        show: {
+            toolbar: true,
+            footer: true
+        },
+        columns: [
+            { field: 'recid', caption: 'Edad', size: '50px', attr: 'align=center' },
+            { field: 'initialBalance', caption: 'Saldo Inicial', size: '100px', resizable: true },
+            { field: 'anualSaving', caption: 'Ahorro Anual', size: '100px', resizable: true },
+            { field: 'anualProfit', caption: 'Ingreso Pasivo', size: '100px', resizable: true },
+            { field: 'taxes', caption: 'Impuestos', size: '100px', resizable: true },
+            { field: 'finalBalance', caption: 'Saldo Final', size: '100px', resizable: true },
+            { field: 'extraSaving', caption: 'Aportación Anual', size: '100px', resizable: true },
+            { field: 'total', caption: 'Total', size: '100px', resizable: true },
+            { field: 'inflation', caption: 'Inflación', size: '100px', resizable: true },
+        ],
+        records: new function(){ return data.anual.records; }
+
+    });
+
+    montlyGrid.w2grid({
+        name: 'monthlyGrid',
+        header: 'Finantial simulator',
+        show: {
+            //toolbar: true,
+            footer: true
+        },
+        columns: [
+            { field: 'recid', caption: 'Edad-Mes', size: '90px', attr: 'align=center' },
+            //{ field: 'month', caption: 'Mes', size: '50px', attr: 'align=center' },
+            { field: 'initialBalance', caption: 'Saldo Inicial', size: '120px', resizable: true },
+            { field: 'profit', caption: 'Rendimiento', size: '120px', resizable: true },
+            { field: 'monthlySaving', caption: 'Ahorro Mensual', size: '120px', resizable: true },
+            { field: 'taxes', caption: 'Impuestos', size: '120px', resizable: true },
+            { field: 'finalBalance', caption: 'Saldo Final', size: '120px', resizable: true },
+
+        ],
+        //searches: [        { field: 'month', caption: 'Mes', type: 'text' },          ],
+        //sortData: [{ field: 'recid', direction: 'ASC' }],
+        records: new function(){ return data.monthly.records; }
+
+    });
+
+    //    montlyGrid.w2render('grid');
+
+    w2ui['anualGrid'].reload();
+    w2ui['monthlyGrid'].reload();
  
- 
-    var data = SIM.monthly(inputData);
- 
-    SIM.drawTable( data.month ,'#result .montly','tbMonth'
+    /*SIM.drawTable( data.month ,'#result .montly','tbMonth'
     ,['Mes','Saldo Inicial', 'Rendimiento','Ahorro mensual', 'impuestos','Saldo Final']);
  
      SIM.drawTable( data.year ,'#result .year','tbYear'
     ,['Edad', 'Saldo Inicial', 'Ahorro Anual', 'Ingreso Pasivo', 'Impuestos','Saldo final', 'Aportación Anual','Total', 'Inflación']);
-    // [ [ 1, 2, 3 ,4], [ 3,4, 5, 6 ], [3,7, 8, 9 ] ];
- 
-   /* var html = '<table class="tbMonth"><thead><tr>...</tr></thead><tbody>';
-    for (var i = 0, len = data.length; i < len; ++i) {
-        html += '<tr>';
-        for (var j = 0, rowLen = data[i].length; j < rowLen; ++j) {
-            html += '<td>' + data[i][j] + '</td>';
-        }
-        html += "</tr>";
-    }
-    html += '</tbody><tfoot><tr>....</tr></tfoot></table>';
- 
-    $().html(html);*/
+    */
  
 }
  
+ SIM.reset = function(){
+    w2ui['anualGrid'].destroy();
+    w2ui['monthlyGrid'].destroy();
  
-SIM.monthly = function (data) {
+ }
+ 
+SIM.simulation = function (data) {
  
     var MONTHS = 12;
     var counter = 0;
@@ -69,16 +121,25 @@ SIM.monthly = function (data) {
     var anualSaving = parseFloat(data.anualSaving);
     var taxes = ((parseFloat(data.taxes))/100);
     var inflation = parseFloat(data.inflation)/100;
+    var initialMonth = data.initialMonth;
  
     var yearResult =[];
     rowYear = [];
     var tasa = ((data.tasa / 100)/364) * 28;
  
     var edad = parseFloat(data.age);
+
+    //alert(data.initialMonth);
+
+    var jResult = {
+        'anual': { 'records': [] },
+        'monthly': { 'records': [] }
+    };
+
+
+
     for (i = 0; i < data.yearsToWork; i++) {
  
-        
-         
         var inc = (inc) ? inc : new Number(data.baseSaving);
          
         var sumSaving = 0;
@@ -86,10 +147,17 @@ SIM.monthly = function (data) {
         var sumtax = 0;
         var sumInlfation = 0;
         var total;
-        
         var lastSaving =  inc;
         
-        for (j = 1; j <= MONTHS; j++) {
+        if(i == 0){
+            j = initialMonth;
+        }
+        else{
+            j=1;
+        }
+
+
+        for (; j <= MONTHS; j++) {
  
             var row = [];
             var profit = parseFloat(inc * tasa);
@@ -99,50 +167,49 @@ SIM.monthly = function (data) {
             sumtax += tax;
             //sumInlfation += sumInlfation + ;
 
-            
-            //var monProfit= (inc + profit + data.montlySaving);
-            //row.push(inc + profit + data.montlySaving);
             total = parseFloat(inc) + profit + montlySaving - tax;
 
-            row.push(j);
-            row.push(accounting.formatMoney(inc));
-            row.push(accounting.formatMoney(profit));
-            row.push(formattedSaving);
-            row.push(accounting.formatMoney(tax,{  format: "%s (%v)" }));
-            row.push(accounting.formatMoney(total));
+            var monthlyEntry ={
+                'recid': (edad+i) + '-'+ j,
+                //'month': j,
+                'initialBalance':  accounting.formatMoney(inc),
+                'profit': accounting.formatMoney(profit),
+                'monthlySaving': formattedSaving,
+                'taxes': accounting.formatMoney(tax,{  format: "%s (%v)" }),
+                'finalBalance': accounting.formatMoney(total)
+            }
 
-            resultMonth.push(row);
+            jResult.monthly.records.push(monthlyEntry);
+           
             inc = new Number(total);
         }
 
         //var yearBalance= sumSaving +sumProfit+anualSaving-sumtax;
-        rowYear.push((edad+i));
-        
-        rowYear.push(accounting.formatMoney(lastSaving));
-
-
-        //rowYear.push(accounting.formatMoney(total));
-        rowYear.push(accounting.formatMoney(sumSaving));
-        rowYear.push(accounting.formatMoney(sumProfit));
-        rowYear.push(accounting.formatMoney(sumtax, {  format: "%s (%v)" }));
-        rowYear.push(accounting.formatMoney(total));
-        rowYear.push(accounting.formatMoney(anualSaving));
-
         inc = inc+ anualSaving;
-        rowYear.push(accounting.formatMoney(inc));
-        rowYear.push(accounting.formatMoney((inc* inflation), {  format: "%s (%v)" }));
-                
-        yearResult.push(rowYear);
 
-        rowYear = [];
+        anualEntry ={
+            'recid': (edad+i), //Edad
+            'initialBalance': accounting.formatMoney(lastSaving), //Saldo Inicial
+            'anualSaving': accounting.formatMoney(sumSaving), //Ahorro Anual
+            'anualProfit': accounting.formatMoney(sumProfit), //Ingreso Pasivo
+            'taxes':accounting.formatMoney(sumtax, {  format: "%s (%v)" }), //Impuestos
+            'finalBalance':accounting.formatMoney(total), //Saldo final
+            'extraSaving': accounting.formatMoney(anualSaving), //Aportación Anual
+            'total': accounting.formatMoney(inc), //Total,
+            'inflation': accounting.formatMoney((inc* inflation), {  format: "%s (%v)" })
+
+        };
+
+        jResult.anual.records.push(anualEntry);
         j = 0;
-        
-        //sumInlfation =0;
  
     }
  
     result.year = yearResult;
     result.month = resultMonth;
+
+    result['anual'] =  jResult.anual;
+    result['monthly'] =  jResult.monthly;
  
     return result;
  
